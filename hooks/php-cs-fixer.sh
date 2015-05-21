2b83ac14fcd2a65f2542ac3cd62045a7d55a59ce
sofakartoffel <marc.paffen@googlemail.com>
2015-05-21 21:40:08 +0200

    allow multiple files for php-cs-fixer

--- hooks/php-cs-fixer.sh
+++ hooks/php-cs-fixer.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 hash php-cs-fixer 2>/dev/null || { echo >&2 "php-cs-fixer is required. Aborting."; exit 1; }
 
-exec php-cs-fixer fix $@ --level=none --fixers=phpdoc_no_package,phpdoc_scalar,single_quote,unused_use,ordered_use,phpdoc_order
-exit $?
+RETURN_CODE=0
+for file in $@
+do
+    eval php-cs-fixer fix $file --level=none --fixers=phpdoc_no_package,phpdoc_scalar,single_quote,unused_use,ordered_use,phpdoc_order
+    if [[ $? != 0 ]]
+    then
+        RETURN_CODE=1
+    fi
+done
+exit $RETURN_CODE
