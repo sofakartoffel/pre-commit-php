89c826b59ff4be759baf69510d22220efeb51950
sofakartoffel <marc.paffen@googlemail.com>
2015-05-21 21:45:57 +0200

    phpdoc_scalar not compatible with JMS\Serializer

--- hooks/php-cs-fixer.sh
+++ hooks/php-cs-fixer.sh
@@ -4,7 +4,7 @@ hash php-cs-fixer 2>/dev/null || { echo >&2 "php-cs-fixer is required. Aborting.
 RETURN_CODE=0
 for file in $@
 do
-    eval php-cs-fixer fix $file --level=none --fixers=phpdoc_no_package,phpdoc_scalar,single_quote,unused_use,ordered_use,phpdoc_order
+    eval php-cs-fixer fix $file --level=none --fixers=phpdoc_no_package,single_quote,unused_use,ordered_use,phpdoc_order
     if [[ $? != 0 ]]
     then
         RETURN_CODE=1
